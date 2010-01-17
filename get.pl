5ff546b3c0f0b0204534b6bb04306fe528713113
Christian Vervoorts <christian@abakus-world.de>
2010-01-17 21:51:24 +0100

    Add logging to the program

--- get.pl
+++ get.pl
@@ -3,7 +3,20 @@ use warnings;
 use strict;
 use LWP::Simple;
 use XML::RSS;
+use Log::Log4perl qw(get_logger);
 #Perl program for parsing a podcast and download the pods
+
+#configuration of the logging
+my $config = q~
+	log4perl.logger				= INFO, LogFile
+	log4perl.appender.LogFile		= Log::Log4perl::Appender::File
+	log4perl.appender.LogFile.filename	= pepol.log
+	log4perl.appender.LogFile.layout	= Log::Log4perl::Layout::PatternLayout
+	log4perl.appender.LogFile.layout.ConversionPattern = [%d] [%p] - %m
+~;
+#initial of the logger
+Log::Log4perl->init( \$config );
+my $logger = get_logger();
 
 #my $url = "http://podcast.wdr.de/quarks.xml";
 #my $url = "http://www.pofacs.de/rss/itunes.xml";
@@ -14,7 +27,7 @@ http://www.pofacs.de/rss/itunes.xml
 
 foreach my $url (@urls) {
 	my $content = get $url
-		or die;
+		or $logger->warn("Could not connect to $url\n");
 	my $rss = XML::RSS->new;
 	$rss->parse($content);
 	my $title = $rss->{channel}->{title};
@@ -24,7 +37,7 @@ foreach my $url (@urls) {
 			my $file = $folder.$title."/".$&;
 			print $file."\n";
 			if (-e $file) {
-				print "File exist\n";
+				$logger->info("File exist: $&\n");
 			} else {
 				getstore($podcast, $file);
 				#print "Need download\n";
