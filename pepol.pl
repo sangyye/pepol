a069f5a687369d4c22e8bc9e97f27ad218b5a68e
Christian Vervoorts <christian@abakus-world.de>
2010-02-01 17:09:48 +0100

    Add CSV-Database to store infos about the downloaded Podcasts

--- pepol.pl
+++ pepol.pl
@@ -4,6 +4,7 @@ use strict;
 use LWP::Simple;
 use XML::RSS;
 use File::Spec;
+use DBI;
 use YAML qw(LoadFile);
 use Log::Log4perl qw(get_logger);
 #Perl program for parsing a podcast and download the pods
@@ -26,6 +27,9 @@ $logger->info("Start pepol\n");
 
 my $file = "";
 $SIG{INT} = \&catch;
+
+#Initial DB
+my $dbh = &connect_db; 
 
 foreach (@{$yaml->{'urls'}}) {
 	chomp;
@@ -52,13 +56,14 @@ foreach (@{$yaml->{'urls'}}) {
 			mkdir($file) unless (-e $file);
 			$file = File::Spec->catfile($file, $&);
 			#print $file."\n";
-			if (-e $file) {
+			if (&in_db($dbh,$&)) {
 				$logger->debug("File exist: $file\n");
 			} else {
 				$logger->info("Start Download: $file\n");
 				getstore($podcast, $file)
 					or $logger->error("Failed Download: $file\n");
 				$logger->info("Finished Download: $file\n");
+				&add_podcast($dbh, $&, $title, $lang || "-", $file);
 				$file = "";
 				$count++;
 			}
@@ -67,6 +72,36 @@ foreach (@{$yaml->{'urls'}}) {
 	$logger->info($rss->{channel}->{title}.": ".$count." File(s) downloaded.\n");
 }
 $logger->info("Stop pepol\n");
+$dbh->disconnect;
+
+sub connect_db {
+	my $dbh = DBI->connect("DBI:CSV:f_dir=./csvdb")
+		or die "Cannot connect: $DBI::errstr";
+	unless (-e File::Spec->catfile("csvdb",$yaml->{'podcastdb'})) {
+		my $sth = $dbh->prepare("CREATE TABLE $yaml->{'podcastdb'} (title VARCHAR(30), channel VARCHAR(30), folder VARCHAR(30), path VARCHAR(60), date VARCHAR(40))");
+		$sth->execute or die "Cannot execute: " . $sth->errstr ();
+		$sth->finish;
+	}
+	return $dbh;
+}
+
+sub add_podcast {
+	my ($dbh, $title, $channel, $folder, $path) = @_;
+	my $time = localtime;
+	$dbh->do("INSERT INTO $yaml->{'podcastdb'} VALUES (?,?,?,?,?)", undef, $title, $channel, $folder, $path, $time);
+}
+
+sub in_db{
+	my ($dbh, $title) = @_;
+	my $sth = $dbh->prepare("SELECT title FROM $yaml->{podcastdb} WHERE title=?");
+	$sth->execute($title);
+	while (my @row = $sth->fetchrow_array) {
+		if ($row[0] eq $title) {
+			return 1;
+		}
+	}
+	return 0;
+}
 
 sub catch {
 	$SIG{INT} = \&catch;
